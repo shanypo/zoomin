59991ed973cad818a3abf2c72c1e5f0361068f7a
shany <shany777@gmail.com>
2021-11-15 10:31:05 +0200

    edit css

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <Router>
       <div className="main-container">
+        <AppHeader />
         <main>
           <Home />
         </main>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { moviesService } from "../services/moviesService";
 import { Toc } from "../cmp/Toc";
 import { MoviesPreview } from "../cmp/MoviesPreview";
+import { IoMdFilm } from "react-icons/io";
+
 export const Home = () => {
   const [movies, setMovies] = useState();
 
@@ -17,12 +19,15 @@ export const Home = () => {
   if (!movies) return <p>Loading...</p>;
   return (
     <div className="home grid">
-      <div>
+      <div className="main-toc flex column">
+        <h4 className="toc-main-title flex align-center">
+          <IoMdFilm /> Movies
+        </h4>
         {movies.map((movie, idx) => (
           <Toc movie={movie} key={idx} />
         ))}
       </div>
-      <div>
+      <div className="main-movies-container flex column">
         {movies.map((movie, idx) => (
           <MoviesPreview movie={movie} key={idx} />
         ))}
